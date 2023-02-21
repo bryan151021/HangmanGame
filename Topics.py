e8452410c904cb8861d3d811027b4fd9fc210f30
Bryan Martinez <bmw10420goat@gmail.com>
2023-02-20 17:51:18 -0800

    adding more words to all the word list and adding new lines to function play

---
+++ Topics.py
@@ -0,0 +1,64 @@
+Classic = ["about", "accept", "according", "action", "baby", "bag", "ball", "before", "campaign", "academy", "center",
+           "charge", "citizen", "yes", "with", "would", "woman", "window", "usually", "turn", "type", "truth",
+           "support", "street", "subject", "station", "space", "skill", "role", "reveal", "response", "relationship",
+           "responsibility", "ready", "product", "program", "private", "once", "option", "open", "official", "outside",
+           "older", "north", "need", "nature", "nation", "lawyer", "late", "learn", "leader", "kid", "kind", "kitchen",
+           "include", "inside", "increase", "impact", "husband", "human", "huge", "hungry", "house", "hospital", "good",
+           "game", "government", "general", "future", "forward", "focus", "follow", "food", "finger", "film", "ever",
+           "example", "even", "event", "exist", "drop", "doctor", "disease", "dream", "drown"]
+
+Movies = ["inception", "goodfellas", "interstellar", "psycho", "parasite", "gladiator", "whiplash", "alien", "oldboy",
+          "coco", "joker", "endgame", "scarface", "heat", "up", "downfall", "batman", "prisoners", "warrior", "logan",
+          "rush", "casablanca", "titanic", "it", "chucky", "rocky", "jaws", "platoon", "braveheart", "goodfellas",
+          "network"]
+
+Video_games = ["minecraft", "fortnite", "tetris", "overwatch", "hades", "bloodborne", "fallout", "inside", "control",
+               "journey", "earthbound", "starcraft", "pokemon", "doom", "portal", "undertale", "bioshock", "halo",
+               "inside", "fifa", "destiny", "dishonored", "journey", "limbo", "borderlands", "braid", "spelunky",
+               "psychonauts", "rez", "everquest", "homeworld", "shenmue", "diablo", "quake", "wipeout", "sonic", "worms",
+               "simcity", "gauntlet"]
+
+Countries = ["afghanistan", "albania", "algeria", "argentina", "australia", "netherlands", "senegal", "ecuador", "qatar",
+             "england", "usa", "iran", "wales", "poland", "mexico", "france", "australia", "tunisia", "denmark", "japan",
+             "spain", "germany", "morocco", "croatia", "belgium", "canada", "brazil", "switzerland", "cameroon", "serbia",
+             "portugal", "uruguay", "ghana", "venezuela", "ukraine", "turkey", "thailand", "sweden", "senegal", "russia",
+             "romania", "peru", "pakistan", "norway", "nigeria", "nepal", "madagascar","monaco", "liberia", "libya",
+             "lebanon", "kenya", "jamaica", "jordan", "italy", "israel", "ireland", "iraq", "iceland", "india", "hungary",
+             "honduras", "guatemala", "greece", "ghana", "gabon", "finland", "egypt", "cuba"]
+
+Hangman_parts = ['''
+       +---+
+       O   |
+      /|\  |
+      / \  |
+          ===''', '''
+       +---+
+       O   |
+      /|\  |
+      /    |
+          ===''', '''
+       +---+
+       O   |
+      /|\  |
+           |
+          ===''', '''
+       +---+
+       O   |
+      /|   |
+           |
+          ===''', '''
+       +---+
+       O   |
+       |   |
+           |
+          ===''', '''
+       +---+
+       O   |
+           |
+           |
+          ===''', '''
+       +---+
+           |
+           |
+           |
+          ===''']
---
+++ main.py
@@ -0,0 +1,111 @@
+from Topics import Classic, Movies, Video_games, Countries, Hangman_parts
+import random
+
+
+# Chooses a random word from the topic the user entered
+def random_word(topic):
+    topic = topic.strip()
+    global count
+    if topic == "CLASSIC":
+        word = random.choice(Classic)
+        word = word.upper()
+        count += 0
+        return play(word)
+    elif topic == "MOVIE":
+        word = random.choice(Movies)
+        word = word.upper()
+        count += 0
+        return play(word)
+    elif topic == "VIDEO GAME":
+        word = random.choice(Video_games)
+        word = word.upper()
+        count += 0
+        return play(word)
+    elif topic == "COUNTRY":
+        word = random.choice(Countries)
+        word = word.upper()
+        count += 0
+        return play(word)
+    else:
+        # This allows the user to input the topic again because the first attempt was invalid
+        print("\nYou entered a invalid Topic, try again.")
+        global again
+        again = (input("Enter one of the following topic's that you will like to play: Classic, Video Game, Country, or Movie: ").upper())
+        count += 1
+        random_word(again)
+
+
+def play(word):
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    hidden_word = "*" * len(word)
+    lives = 6
+    # Decides whether to use the first topic the user entered or the second one
+    b = topic if count == 0 else again
+    print(f"\nTopic: {b.strip()}", end='')
+    print(Hangman_parts[lives])
+    print(f"Secret word: {hidden_word}")
+    print(f"Lives = {lives}")
+    while not guessed and lives > 0:
+        user_guess = input("\nEnter a guess, this can either be a letter or word: ").upper()
+        # user guess is a letter
+        if len(user_guess) == 1:
+            # checks if user guess is in guessed letters
+            if user_guess in guessed_letters:
+                print(f"You already guessed this letter: {user_guess}")
+            # checks if user guess is not in the word
+            elif user_guess not in word:
+                guessed_letters.append(user_guess)
+                lives -= 1
+                print(f"Sorry but {user_guess} is not in the word")
+            # means user guess is in the word
+            else:
+                print(f"Correct {user_guess} is in the word")
+                guessed_letters.append(user_guess)
+                # figure a way to update the hidden word to reveal the correct letters the user guessed
+                ...
+                if "*" not in hidden_word:
+                    guessed = True
+        # user guess is a word
+        elif len(user_guess) == len(word):
+            # checks if user guess is in guessed words
+            if user_guess in guessed_words:
+                print(f"You already guessed this word: {user_guess}")
+            # checks if user guess is not in the word
+            elif user_guess != word:
+                guessed_words.append(user_guess)
+                lives -= 1
+                print(f"Sorry but {user_guess} is not the word")
+            # means user guess is in word
+            else:
+                hidden_word = word
+                guessed = True
+        # user guess is invalid
+        else:
+            print("You entered a invalid guess.")
+        b = topic if count == 0 else again
+        print(f"\nTopic: {b.strip()}", end='')
+        print(Hangman_parts[lives])
+        print(f"hidden word: {hidden_word}")
+        print(f"lives remaining: {lives}")
+        print(f"Guessed letters: {guessed_letters}")
+        print(f"Guessed words: {guessed_words}")
+        if lives == 0:
+            print("\nYou died")
+            print("Sorry but you ran out of lives")
+        elif guessed:
+            print("You guessed the word, congratulation!!")
+        else:
+            continue
+
+
+def main(topic):
+    random_word(topic)
+
+
+# This should be the first thing you see and this starts the game
+print("\nLet's play Hangman!!")
+topic = input("Enter one of the following topic's that you will like to play: Classic, Video Game, Country, or Movie: ").upper()
+count = 0
+main(topic)
